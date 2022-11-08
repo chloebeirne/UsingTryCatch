2dbb3bd2bd7d65a1abfce05be4935cb31a40df01
chloebeirne <g00399850@atu.ie>
2022-11-08 13:37:57 +0000

    Created a code

--- src/main/java/ie/atu/week7.java
+++ src/main/java/ie/atu/week7.java
@@ -1,4 +1,17 @@
 package ie.atu;
 
+import java.util.Scanner;
+
 public class week7 {
+    public static void main(String[] args){
+        System.out.println("Please enter a number between 0 - 9: ");
+        Scanner myscan = new Scanner (System.in);
+        int myNum = myscan.nextInt();
+            if (myNum <= 9 && myNum > 0){
+                System.out.println("You entered: ", + myNum);
+            }
+            else{
+                System.out.println("Not a valid number");
+            }
+    }
 }
