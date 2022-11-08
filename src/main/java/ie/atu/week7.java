bb4dbcc485175472db9f4b6ec8b77b0f0428ef44
chloebeirne <g00399850@atu.ie>
2022-11-08 13:59:32 +0000

    Created a try catch

--- src/main/java/ie/atu/week7.java
+++ src/main/java/ie/atu/week7.java
@@ -1,17 +1,21 @@
 package ie.atu;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class week7 {
     public static void main(String[] args){
         System.out.println("Please enter a number between 0 - 9: ");
         Scanner myscan = new Scanner (System.in);
-        int myNum = myscan.nextInt();
-            if (myNum <= 9 && myNum > 0){
-                System.out.println("You entered: ", + myNum);
-            }
-            else{
+        try {
+            int myNum = myscan.nextInt();
+            if (myNum <= 9 && myNum > 0) {
+                System.out.println("You entered: " + myNum);
+            } else {
                 System.out.println("Not a valid number");
             }
+        }catch(InputMismatchException ime){
+            System.out.println("Error found");
+        }
     }
 }
