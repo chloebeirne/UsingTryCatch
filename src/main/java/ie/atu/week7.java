621b9810b88a6c87558a018d86d4349351dbd5ed
chloebeirne <g00399850@atu.ie>
2022-11-08 14:18:34 +0000

    Created a while true loop

--- src/main/java/ie/atu/week7.java
+++ src/main/java/ie/atu/week7.java
@@ -7,15 +7,19 @@ public class week7 {
     public static void main(String[] args){
         System.out.println("Please enter a number between 0 - 9: ");
         Scanner myscan = new Scanner (System.in);
-        try {
-            int myNum = myscan.nextInt();
-            if (myNum <= 9 && myNum > 0) {
-                System.out.println("You entered: " + myNum);
-            } else {
-                System.out.println("Not a valid number");
+        while(true){
+            try {
+                int myNum = myscan.nextInt();
+                if (myNum <= 9 && myNum > 0) {
+                    System.out.println("You entered: " + myNum);
+                    break;
+                } else {
+                    System.out.println("Not a valid number, enter a number between 0 - 9: ");
+                }
+            } catch (InputMismatchException ime) {
+                System.out.println("Error found, insert an integer: ");
+                myscan.next();
             }
-        }catch(InputMismatchException ime){
-            System.out.println("Error found");
         }
     }
 }
